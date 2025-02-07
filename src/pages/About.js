ec9ae3be27cbd7eb61c5047693aa3134b4e8c88f
Maximum-VI <virinatsiri323@gmail.com>
2025-02-07 11:36:42 +0700

    Second Commit

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="container mt-4">
+      <h1>About Us</h1>
+      <p>Lanna Polytechnic College is a leading educational institution in Chiang Mai, Thailand.</p>
+      <h3>Vision & Goals</h3>
+      <p>Our IT department aims to produce skilled professionals who are ready to contribute to the growing tech industry.</p>
+    </div>
+  );
+}
+
+export default About;
+        
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container mt-4">
+      <h1>Welcome to the IT Department</h1>
+      <p>Learn more about our department and the programs we offer.</p>
+      <img src="https://img.freepik.com/free-photo/group-young-business-people-working-office_158595-5210.jpg?semt=ais_hybrid" alt="Activities" className="img-fluid" />
+      <p>Our department's philosophy is to develop skilled IT professionals ready for the industry.</p>
+    </div>
+  );
+}
+
+export default Home;
+  
